6fc24fc66dab4c67cebe492bd6c994892b7e8bf4
Bellmere <therise00@gmail.com>
2022-11-06 20:44:38 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,16 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const divEl = document.createElement('div').classList.add('gallery__item');
+const linkEl = document.createElement('a').classList.add('gallery__link');
+let imgEl = document.createElement('img').classList.add('gallery__image');
+
+divEl.innerHTML = linkEl;
+
+const newGallery = galleryItems.map(item => {
+    imgEl = `<img src=${item.preview} alt=${item.description} data-source=${item.original}></img>`;
+}).join('');
+
+// galleryEl.insertAdjacentElement('afterbegin', newGallery);
