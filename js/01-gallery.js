c7d40af1f11b386de1e017cc8c2b1eff48819cb5
Bellmere <therise00@gmail.com>
2022-11-07 22:51:46 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,33 +4,59 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const galleryEl = document.querySelector('.gallery');
-const arr = [];
 
-
-galleryItems.forEach(el => {
-    const divEl = document.createElement('div');
-    divEl.className = 'gallery__item';
-    const linkEl = document.createElement('a');
-    linkEl.className = 'gallery__link';
-    linkEl.href = el.original;
-    const imgEl = document.createElement('img');
-    imgEl.className = 'gallery__image';
-    imgEl.src = el.preview;
-    imgEl.alt = el.description;
-    imgEl.setAttribute('data-source', el.original);
-
-    divEl.append(linkEl);
-    linkEl.append(imgEl);
-    arr.push(divEl);
+const newGallery = galleryItems.map(item => {
+    
 });
 
-galleryEl.append(...arr);
 
-galleryEl.addEventListener('click', event => {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
-        return ;
-    }
 
-    const selectImg = event.target.dataset.source;
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const arr = [];
+
+
+// galleryItems.forEach(el => {
+//     const divEl = document.createElement('div');
+//     divEl.className = 'gallery__item';
+//     const linkEl = document.createElement('a');
+//     linkEl.className = 'gallery__link';
+//     linkEl.href = el.original;
+//     const imgEl = document.createElement('img');
+//     imgEl.className = 'gallery__image';
+//     imgEl.src = el.preview;
+//     imgEl.alt = el.description;
+//     imgEl.setAttribute('data-source', el.original);
+
+//     divEl.append(linkEl);
+//     linkEl.append(imgEl);
+//     arr.push(divEl);
+// });
+
+// galleryEl.append(...arr);
+
+// galleryEl.addEventListener('click', event => {
+//     event.preventDefault();
+//     if (event.target.nodeName !== 'IMG') {
+//         return ;
+//     }
+
+//     const selectImg = event.target.dataset.source;
+// })
