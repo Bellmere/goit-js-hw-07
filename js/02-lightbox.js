d6130cfa0aa0a23abaf334d27216cc979ca13326
Bellmere <therise00@gmail.com>
2022-11-08 21:56:47 +0200

    2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+const newGallery = createElement(galleryItems);
+
+galleryEl.insertAdjacentHTML('afterbegin', newGallery);
+
+function createElement(gallery) {
+    return gallery.map(({ preview, original, destription }) => {
+        return `
+            <li class="gallery__item">
+            <a class="gallery__item" 
+            href="${original}">
+            <img class="gallery__image" 
+            src="${preview}" 
+            alt="${destription}" />
+            </a>
+            </li>
+        `;
+    }).join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+     /* options */ 
+     captionDelay: 300,
+     animationSpeed: 300,
+
+    });
